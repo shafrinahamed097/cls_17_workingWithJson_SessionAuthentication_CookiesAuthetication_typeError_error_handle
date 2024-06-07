4d2512aeb005847c6feffc6367345477d6659714
shafrinahamed97 <shafrinahamedjob@gmail.com>
2024-06-07 14:11:52 +0600

    JSON to Array Convert, this covert result in fontend

--- what_is_file_get_contents().php
+++ what_is_file_get_contents().php
@@ -8,6 +8,14 @@ if($_SERVER['REQUEST_METHOD']=="POST"){
   $PlainTestJSON = file_get_contents("php://input");
   $PlainTestArray=json_decode($PlainTestJSON,true);
   $PlainArray['num1'];
-  echo $PlainTestArray['num1']+$PlainTestArray['num2'];
+  $sum=$PlainTestArray['num1']+$PlainTestArray['num2'];
+  $sub=$PlainTestArray['num1']-$PlainTestArray['num2'];
+  $multi=$PlainTestArray['num1']*$PlainTestArray['num2'];
+  $div=$PlainTestArray['num1']/$PlainTestArray['num2'];
+  
+
+  $result=array("sum"=>$sum,"sub"=>$sub,"multi"=>$multi,"div"=>$div);
+  echo json_encode($result);
+
     
 }
