ec56e4f4ec2780d27e9cde2a3ea578c3421d0910
shafrinahamed97 <shafrinahamedjob@gmail.com>
2024-06-07 13:57:43 +0600

    JSON to Array Convert

--- what_is_file_get_contents().php
+++ what_is_file_get_contents().php
@@ -6,5 +6,8 @@
 
 if($_SERVER['REQUEST_METHOD']=="POST"){
   $PlainTestJSON = file_get_contents("php://input");
-    echo $PlainTestJSON;
+  $PlainTestArray=json_decode($PlainTestJSON,true);
+  $PlainArray['num1'];
+  echo $PlainTestArray['num1']+$PlainTestArray['num2'];
+    
 }
